b007c4fbcd1ef8e23f1016079f7c02f213298b88
Haris Dharma Putra <58357619+HarizDharma@users.noreply.github.com>
2020-09-15 15:32:05 +0700

    Initial Commit
    Initial Commit

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
   $server = "localhost";
   $db_username="root";
-  $db_password ="dedsec";
+  $db_password ="";
   $db = "chat";
 
    $con = mysqli_connect("$server","$db_username","$db_password","$db") or die ("connection Unsuccessfull");
